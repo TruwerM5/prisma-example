8720794fbdac380b90406df783106582c45ea51f
halimov <halimov@infomatika.ru>
2026-07-19 20:21:35 +0300

    update products seed

--- prisma/seeds/products.seed.ts
+++ prisma/seeds/products.seed.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from 'src/generated/prisma/client';
 import { Prisma } from 'src/generated/prisma/client';
 
-// const prisma = new PrismaClient();
 const products: Prisma.ProductCreateInput[] = [
     {
         name: '1984',
@@ -13,6 +12,7 @@ const products: Prisma.ProductCreateInput[] = [
         },
         productDetails: {
             create: {   
+                category: 'books',
                 price: 10,
                 author: 'George Orwell',
                 description: "Nineteen Eighty-Four is a dystopian speculative fiction novel by the English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final completed book. Thematically, it centres on totalitarianism, mass surveillance and repressive regimentation of people and behaviours.",
@@ -29,6 +29,7 @@ const products: Prisma.ProductCreateInput[] = [
         },
         productDetails: {
             create: {
+                category: 'books',
                 price: 8.63,
                 author: 'Ray Bradbury',
                 description: "Ray Bradbury's 1957 semi-autobiographical novel Dandelion Wine is set in the summer of 1928 in the fictional Green Town, Illinois. The story follows 12-year-old Douglas Spaulding as he discovers the magic of small-town life, grapples with his own mortality, and witnesses the bittersweet realities of adulthood",
