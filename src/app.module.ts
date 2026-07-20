eba67f292ad3a601b924487e2fd65020aaa56dd1
halimov <halimov@infomatika.ru>
2026-07-20 14:33:29 +0300

    remove redis service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RedisSerivce } from './redis/index.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
@@ -10,6 +9,6 @@ import { ProductsModule } from './products/products.module';
 @Module({
   imports: [UserModule, AuthModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService, RedisSerivce, PrismaService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { RedisSerivce } from './redis/index.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly redisService: RedisSerivce) {}
-
-  async getHello(): Promise<string> {
-    await this.redisService.set('user:1', 'John');
+  getHello(): string {
     return 'Hello World';
-  }
-
-  async getUser() {
-    const value = await this.redisService.get('user:1');
-    const bike = await this.redisService.get('bike:1');
-    return { value, bike };
   }
 }
