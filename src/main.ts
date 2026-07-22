c360a16e3a606406563617bb8f99204e957a1856
halimov <halimov@infomatika.ru>
2026-07-22 17:50:24 +0300

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Shop API')
+  .setVersion('1.0')
+  .addTag('shop')
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.use(cookieParser.default());
   app.enableCors({
     origin: 'http://localhost:3000',
