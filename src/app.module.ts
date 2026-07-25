ccf95829e024135d26c885423aa0a3a0d67b63fd
halimov <halimov@infomatika.ru>
2026-07-25 23:03:13 +0300

    init cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ProductsModule, OrdersModule],
+  imports: [UserModule, AuthModule, ProductsModule, OrdersModule, CartModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
